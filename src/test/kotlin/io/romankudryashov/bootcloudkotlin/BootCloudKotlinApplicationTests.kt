850d2846d44fe5052f3c7b536576fd7419992c09
Roman Kudryashov <rskudryashov@gmail.com>
2018-02-17 16:33:29 +0300

    Initial commit

---
+++ src/test/kotlin/io/romankudryashov/bootcloudkotlin/BootCloudKotlinApplicationTests.kt
@@ -0,0 +1,16 @@
+package io.romankudryashov.bootcloudkotlin
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BootCloudKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
