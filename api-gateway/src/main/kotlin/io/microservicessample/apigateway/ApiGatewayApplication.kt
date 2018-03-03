b5f5fb5c3182a4ea0a0fd4ab4fd23a1eb065455d
Roman Kudryashov <rskudryashov@gmail.com>
2018-03-03 11:19:18 +0300

    Remove @EnableDiscoveryClient

--- api-gateway/src/main/kotlin/io/microservicessample/apigateway/ApiGatewayApplication.kt
+++ api-gateway/src/main/kotlin/io/microservicessample/apigateway/ApiGatewayApplication.kt
@@ -2,10 +2,8 @@ package io.microservicessample.apigateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
-@EnableDiscoveryClient
 class ApiGatewayApplication
 
 fun main(args: Array<String>) {
--- greeting-service/src/main/kotlin/io/microservicessample/greetingservice/GreetingServiceApplication.kt
+++ greeting-service/src/main/kotlin/io/microservicessample/greetingservice/GreetingServiceApplication.kt
@@ -2,10 +2,8 @@ package io.microservicessample.greetingservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
-@EnableDiscoveryClient
 class GreetingServiceApplication
 
 fun main(args: Array<String>) {
