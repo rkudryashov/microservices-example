05a9280e1a36412978c94eb7f11be21051e64d6f
Roman Kudryashov <rskudryashov@gmail.com>
2018-12-16 21:11:58 +0300

    Generate XML code coverage report instead of HTML

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ tasks.register<JacocoReport>("jacocoRootReport") {
     classDirectories.setFrom(subprojects.map { it.buildDir.resolve("classes") })
     executionData(subprojects.map { it.buildDir.resolve("jacoco/test.exec") })
     reports {
-        html.destination = File("$buildDir/jacoco/testCoverageReport")
+        html.isEnabled = false
+        xml.isEnabled = true
+        xml.destination = File("$buildDir/jacoco/report.xml")
     }
 }
