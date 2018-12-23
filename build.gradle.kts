65b8af76c851368fd54ab55239954071f935ee5a
Roman Kudryashov <rskudryashov@gmail.com>
2018-12-23 22:32:23 +0300

    Setup task for test coverage verification

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,37 @@ repositories {
 }
 
 plugins {
+    java
     jacoco
 }
 
-tasks.register<JacocoReport>("jacocoRootReport") {
-    classDirectories.setFrom(subprojects.map { it.buildDir.resolve("classes/kotlin/main") })
-    executionData(subprojects.map { it.buildDir.resolve("jacoco/test.exec") })
-    reports {
-        html.isEnabled = false
-        xml.isEnabled = true
-        xml.destination = File("$buildDir/jacoco/report.xml")
+tasks {
+    val subProjectsClasses = subprojects.map { it.buildDir.resolve("classes/kotlin/main") }
+    val subProjectsExecutionData = subprojects.map { it.buildDir.resolve("jacoco/test.exec") }
+
+    register<JacocoReport>("jacocoRootReport") {
+        classDirectories.setFrom(subProjectsClasses)
+        executionData(subProjectsExecutionData)
+        reports {
+            html.isEnabled = false
+            xml.isEnabled = true
+            xml.destination = File("$buildDir/jacoco/report.xml")
+        }
+    }
+
+    register<JacocoCoverageVerification>("jacocoCoverageVerification") {
+        classDirectories.setFrom(subProjectsClasses)
+        executionData(subProjectsExecutionData)
+        violationRules {
+            rule {
+                limit {
+                    minimum = BigDecimal.valueOf(0.70)
+                }
+            }
+        }
+    }
+
+    "check" {
+        dependsOn("jacocoCoverageVerification")
     }
 }
